9712a6d15412495075d77cd98c73899bbd4cba24
edyquintero <edyquintero882@gmail.com>
2025-03-21 17:07:52 -0500

    Starting Udemy Java Course 🟥🟥

---
+++ appweb/src/main/java/com/edy/curso/springboot/webapp/springbootweb/AppwebApplication.java
@@ -0,0 +1,13 @@
+package com.edy.curso.springboot.webapp.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppwebApplication.class, args);
+    }
+
+}
---
+++ appweb/src/test/java/com/edy/curso/springboot/webapp/springbootweb/AppwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edy.curso.springboot.webapp.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
